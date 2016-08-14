79532d1bd4af47b1894304ac041db00ce7c30640
ZhangHanming <zhang.hanming.official@gmail.com>
2016-08-15 00:20:15 +0800

    add gitignore file

--- public/javascripts/facebook.js
+++ public/javascripts/facebook.js
@@ -1,38 +1,38 @@
 window.fbAsyncInit = function() {
-    FB.init({
-        appId: '833829723419057',
-        xfbml: true,
-        version: 'v2.7'
-    });
+  FB.init({
+    appId: '833829723419057',
+    xfbml: true,
+    version: 'v2.7'
+  });
 
-    FB.getLoginStatus(function(response) {
-        if (response.status === 'connected') {
-            FB.api('/me', {
-                fields: 'name'
-            }, function(response) {
-                document.getElementById("welcome_message").innerHTML = "Welcome " + response.name;
-            });
-        }
-    });
+  FB.getLoginStatus(function(response) {
+    if (response.status === 'connected') {
+      FB.api('/me', {
+        fields: 'name'
+      }, function(response) {
+        document.getElementById("welcome_message").innerHTML = "Welcome " + response.name;
+      });
+    }
+  });
 
-    FB.Event.subscribe("auth.login", function(response) {
-        window.location = window.location + '?id=' + FB.getAuthResponse().userID;
+  FB.Event.subscribe("auth.login", function(response) {
+    window.location = window.location + '?id=' + FB.getAuthResponse().userID;
 
-    });
+  });
 
-    FB.Event.subscribe("auth.logout", function(response) {
-        location.reload();
-    });
+  FB.Event.subscribe("auth.logout", function(response) {
+    location.reload();
+  });
 
 };
 
 (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-        return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
