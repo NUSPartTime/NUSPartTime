b39097428075d630de79babdf63899672f5f91b3
WYHNUS <wangyanhao1993@163.com>
2016-08-18 02:54:38 +0800

    add testing data file

---
+++ sql/testing_data.sql
@@ -0,0 +1,40 @@
+-- insert into DB via command line for testing
+
+insert into Users values
+-- id, name, address, phone, email, description, registerDate
+(1, "Test User 001", "Clementi", "90785634", "abc@testing.com", "I'm a good test player.", "2016-08-18", "2016-08-18", "2016-08-18"), 
+(2, "Test User 002", "Clementi", "12345678", "xyz@testing.com", "I'm a good test player too.", "2016-08-18", "2016-08-18", "2016-08-18"), 
+(4, "Test User 004", "", "", "", "", "2016-08-18", "2016-08-18", "2016-08-18")
+;
+
+insert into Students values
+-- id, matric
+(1, "A0123456B", "2016-08-18", "2016-08-18"),
+(4, "A0134567C", "2016-08-18", "2016-08-18")
+;
+
+insert into Employers values
+-- id
+(2, "2016-08-18", "2016-08-18")
+;
+
+insert into Companies values
+-- id, name, address, email, description, registerDate
+(1, "Google", "4 Siglap", "www.google.com", "Diao de bu xing", "2016-08-18", "2016-08-18", "2016-08-18")
+;
+
+insert into CompanyContacts values
+-- id, company_id, employer_id
+(1, "2016-08-18", "2016-08-18", 2, 1)
+;
+
+insert into Jobs values
+-- id, company_id, title, description, status, salary, applicationDeadline, deadline
+(1, "Front end developer", 100, "GLHF", 1, "2016-09-18", "2016-11-18", "2016-08-18", "2016-08-18", 1)
+;
+
+insert into Interests values
+-- id, student_id, job_id, status
+(1, 1, "2016-08-18", "2016-08-18", 4, 1),
+(2, 0, "2016-08-18", "2016-08-18", 1, 1)
+;
