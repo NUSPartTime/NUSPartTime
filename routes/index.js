6dc8c344b6cbb429fa83c7a73b7af5e5973fcc77
WYHNUS <wangyanhao1993@163.com>
2016-08-22 16:28:52 +0800

    move vendor js scripts into a seperate folder; add dummy 1st time registration page in index.html

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	var should_register = false;
 	if (req.session.user_id) {
 		// logged in but haven't chosen student or company yet
-		console.log("req.session.user_id");
+		// ask user to choose
+		should_register = true;
 	} else {
 		// not logged in yet
 	}
     res.render('index', {
-        title: 'NUS Part-time'
+        title: 'NUS Part-time',
+        show_register_window: should_register
     });
 });
 
