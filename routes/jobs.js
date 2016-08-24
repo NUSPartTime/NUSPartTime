4172d6043103d6c4c29a8e6c5232be8265d8726d
Yang Zhuohan <billstark1996@gmail.com>
2016-08-24 21:39:45 +0800

    finish implementing view jobs/edit jobs

--- routes/jobs.js
+++ routes/jobs.js
@@ -39,7 +39,99 @@ router.get('/:job_id/details', function(req, res) {
     res.render('job', {
       job: job,
       applicationStatus: applicationStatus
+    });
+  });
+});
+
+/* This is for company to view job status */
+router.get('/:job_id/view', function(req, res, next) {
+  var sess = req.session;
+  var userId = sess.user_id;
+  models.sequelize.Promise.all([
+    models.StudentJob.findAll({
+      where: {
+
+        // Use job id to find the join tables.
+        jobId: req.params.job_id
+      },
+      include: [models.Student]
+    }),
+    models.Job.findAll({
+      where: {
+        
+        // Also use job id to find the particular job
+        id: req.params.job_id
+      },
+      include: [models.Company]
+    }),
+    models.CompanyContact.findAll({
+      where: {
+        employerId: userId
+      },
+      include: [models.Company]
+    }),
+
+  ]).spread(function(all_studentJobs, all_jobs, all_companyContacts) {
+
+    if (typeof(all_jobs[0]) != "undefined") {
+      var job = all_jobs[0];
+      var all_students = [];
+      var companies = [];
+      for(var studentJob of all_studentJobs) {
+        if(studentJob.status !== 0) {
+          all_students.push(studentJob.Student);
+        }
+      }
+
+      // fetch all companies
+      for(var companyContact of all_companyContacts) {
+        var company = companyContact.Company;
+        companies[company.id] = company.name;
+      }
+
+      res.render('edit_job', {
+        job: job,
+        students: all_students,
+        title: "Applicants",
+        companies: companies
+      });
+    } else {
+      res.render('user_error', {
+        message: "The job requested doesn't exist."
+      });
+    }
+
+    // we create an array to store all the students who have successfully apppied for this job
+    
+  });
+});
+
+/* This is for companies to update a job */
+router.post('/:job_id/edit', function(req, res) {
+  var sess = req.session;
+  models.sequelize.Promise.all([
+    models.Job.findAll({
+      where: {
+        
+        // Also use job id to find the particular job
+        id: req.params.job_id
+      },
     })
+  ]).spread(function(all_jobs) {
+    if(typeof(all_jobs[0]) != "undefined") {
+      job = all_jobs[0];
+      job.update({
+        companyId: req.body.companyId,
+        title: req.body.title,
+        status: req.body.status,
+        salary: req.body.salary,
+        description: req.body.description,
+        applicationDeadline: req.body.application_deadline,
+        deadline: req.body.deadline
+      }).then(function(){
+        res.redirect('/jobs/' + req.params.job_id + '/view');
+      });
+    }
   });
 });
 
