54f550a64afc5a2d5aa965ede99c7c679344b1cc
WYHNUS <wangyanhao1993@163.com>
2016-08-29 23:16:23 +0800

    change index.html indentation

--- public/app/components/studentRegistration/studentRegistrationController.js
+++ public/app/components/studentRegistration/studentRegistrationController.js
@@ -3,6 +3,9 @@
 angular.module("nusPartimeApp").controller("studentRegistrationController", ["$scope", 
 	function($scope) {
 		$scope.register = function() {
+			console.log($scope.matricNumber);
+			// suppose to check validity of matric number
+			// then register
 			
 		}
 	}]);
