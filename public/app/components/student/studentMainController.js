b09d80ff95add30e8fdb7c6d792b38f2adf62ab2
WYHNUS <wangyanhao1993@163.com>
2016-08-30 13:56:43 +0800

    config tow-way-binding in student page

--- public/app/components/student/studentMainController.js
+++ public/app/components/student/studentMainController.js
@@ -4,6 +4,8 @@ angular.module("nusPartimeApp").controller("studentMainController",
 	["$scope", "$location", "Session", "AuthService", "DataRetrievalService",
 	function($scope, $location, Session, AuthService, DataRetrievalService) {
 		$scope.catJobsArray = [];
+		$scope.allJobsArray = [];
+		$scope.displayedJobs = [];
 
 		AuthService.autoLogin().then(function(res) {
 			if (res.isRegistered && !Session.isStudent) {
@@ -14,13 +16,18 @@ angular.module("nusPartimeApp").controller("studentMainController",
 		DataRetrievalService.getAllJobs().then(function(res) {
 			console.log(res);
 			$scope.catJobsArray = res;
-		})
+			for (var catJobs of res) {
+				$scope.allJobsArray = $scope.allJobsArray.concat(catJobs.jobs);
+			}
+			$scope.showAllContent();
+			$("#all-jobs-button").focus();
+		});
 
-		$scope.showAllContent() = function() {
-
+		$scope.showAllContent = function() {
+			$scope.displayedJobs = $scope.allJobsArray;
 		}
 
-		$scope.showContent() = function(index) {
-
+		$scope.showContent = function(index) {
+			$scope.displayedJobs = $scope.catJobsArray[index].jobs;
 		}
 	}]);
