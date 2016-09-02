1a2a530a8a382c249fa3d0480b0744f8ed6c3e94
Yang Zhuohan <billstark1996@gmail.com>
2016-09-02 20:48:39 +0800

    alerts

--- public/app/components/jobEdit/jobEditController.js
+++ public/app/components/jobEdit/jobEditController.js
@@ -1,8 +1,8 @@
 "use strict";
 
 angular.module("nusPartimeApp").controller("jobEditController",
-	["$scope", "$location", "RegistrationService", "AuthService", "Session", "JobService",
-	function($scope, $location, RegistrationService, AuthService, Session, JobService) {
+	["$scope", "$filter", "$location", "RegistrationService", "AuthService", "Session", "JobService",
+	function($scope, $filter, $location, RegistrationService, AuthService, Session, JobService) {
 		AuthService.autoLogin()
         .then(function(){
             JobService.getJobApplicationForm(Session.userId).then(function(res){
@@ -21,8 +21,17 @@ angular.module("nusPartimeApp").controller("jobEditController",
 
         $scope.currentJob = JobService.getCurrentJob();
 		$scope.updateJob = function(){
+			swal("Success", "Job info updated!", "success");
             JobService.updateJob($scope.currentJob).then(function(res){
                 $location.path("/job/"+$scope.currentJob.id);
             });
         }
+
+		$scope.$watch("currentJob.applicationDeadline", function (newValue) {
+			$scope.currentJob.applicationDeadline = $filter('date')(newValue, 'MMMM dd yyyy')
+		});
+
+		$scope.$watch("currentJob.deadline", function (newValue) {
+			$scope.currentJob.deadline = $filter('date')(newValue, 'MMMM dd yyyy')
+		});
 	}]);
