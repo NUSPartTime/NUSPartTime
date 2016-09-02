c9bb18856d07456c0ee0ffde55892c8e33fc5cf3
Bohan Huang <weepineapple@gmail.com>
2016-09-02 12:22:43 +0800

    Created new column "resume" in student model and migration file

--- models/student.js
+++ models/student.js
@@ -1,7 +1,8 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var Student = sequelize.define('Student', {
-    matric: DataTypes.STRING
+    matric: DataTypes.STRING,
+    resume: DataTypes.STRING
   }, {
     classMethods: {
       associate: function(models) {
